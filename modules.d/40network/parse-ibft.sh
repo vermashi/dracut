3860f2788108339ef5de9667e0472d1323c13439
Harald Hoyer <harald@redhat.com>
2015-05-28 13:56:44 +0200

    ibft: correct device name

--- modules.d/40network/parse-ibft.sh
+++ modules.d/40network/parse-ibft.sh
@@ -19,6 +19,8 @@ for p in $(getargs ip=); do
             unset mask
             unset hostname
             unset vlan
+            unset dns1
+            unset dns2
             [ -e ${iface}/mac ] || continue
             ifname_mac=$(read a < ${iface}/mac; echo $a)
             [ -z "$ifname_mac" ] && continue
@@ -29,6 +31,7 @@ for p in $(getargs ip=); do
                     break
                 fi
             done
+
             if [ -z "$dev" ]; then
                 ifname_if=ibft$num
                 num=$(( $num + 1 ))
@@ -36,7 +39,28 @@ for p in $(getargs ip=); do
                 dev=$ifname_if
             fi
 
+            if [ -e ${iface}/vlan ]; then
+                vlan=$(read a < ${iface}/vlan; echo $a)
+                if [ "$vlan" -ne "0" ]; then
+                    case "$vlan" in
+                        [0-9]*)
+                            [ -e /tmp/net.${dev}.${vlan}.has_ibft_config ] && continue
+                            echo "vlan=$dev.$vlan:$dev"
+                            dev="${dev}.${vlan}"
+                            ;;
+                        *)
+                            [ -e /tmp/net.${vlan}.has_ibft_config ] && continue
+                            echo "vlan=$vlan:$dev"
+                            dev="${vlan}"
+                            ;;
+                    esac
+                fi
+            fi
+
+            [ -e /tmp/net.${dev}.has_ibft_config ] && continue
+
             [ -e ${iface}/dhcp ] && dhcp=$(read a < ${iface}/dhcp; echo $a)
+
             if [ -n "$dhcp" ]; then
                 echo "ip=$dev:dhcp"
             else
@@ -46,21 +70,14 @@ for p in $(getargs ip=); do
                 [ -e ${iface}/subnet-mask ] && mask=$(read a < ${iface}/subnet-mask; echo $a)
                 [ -e ${iface}/hostname ] && hostname=$(read a < ${iface}/hostname; echo $a)
                 [ -n "$ip" ] && echo "ip=$ip::$gw:$mask:$hostname:$dev:none"
+
+                [ -e ${iface}/primary-dns ] && dns1=$(read a < ${iface}/primary-dns; echo $a) \
+                                                   && echo "nameserver=${dns1}"
+                [ -e ${iface}/secondary-dns ] && dns2=$(read a < ${iface}/secondary-dns; echo $a) \
+                                                   && echo "nameserver=${dns2}"
             fi
 
-            if [ -e ${iface}/vlan ]; then
-                vlan=$(read a < ${iface}/vlan; echo $a)
-                if [ "$vlan" -ne "0" ]; then
-                    case "$vlan" in
-                        [0-9]*)
-                            echo "vlan=$dev.$vlan:$dev"
-                            ;;
-                        *)
-                            echo "vlan=$vlan:$dev"
-                            ;;
-                    esac
-                fi
-            fi
+            echo $mac > /tmp/net.${dev}.has_ibft_config
         done
     ) >> /etc/cmdline
     # reread cmdline
